366afad48fb2d31374ddee292711e17b02a29d0c
Shuaib-mahmoud <85953008+Shuaib-mahmoud@users.noreply.github.com>
2021-10-22 19:50:15 +0200

    my first commit

---
+++ main.js
@@ -0,0 +1,75 @@
+'use strict'
+if (localStorage.getItem('ourProducts') != null) {
+  productsContainer = JSON.parse(localStorage.getItem('ourProducts'));
+  displayProduct()
+}
+else {
+  var productsContainer = [];
+}
+var productNameInput = document.getElementById('productName');
+var productPriceInput = document.getElementById('productPrice');
+var productCategoryInput = document.getElementById('productCategory');
+var productDescInput = document.getElementById('productDesc');
+
+
+function addProduct() {
+  var ourProduct = {
+    name: productNameInput.value,
+    price: productPriceInput.value,
+    category: productCategoryInput.value,
+    description: productDescInput.value,
+  }
+  console.log(ourProduct);
+  productsContainer.push(ourProduct)
+  localStorage.setItem('ourProducts', JSON.stringify(productsContainer))
+  // clearForm()
+  displayProduct()
+}
+
+function clearForm() {
+  productNameInput.value = '';
+  productPriceInput.value = '';
+  productCategoryInput.value = '';
+  productDescInput.value = '';
+}
+
+function displayProduct() {
+  var dumpProducts = ` `
+  for (var i = 0; i < productsContainer.length; i++) {
+    dumpProducts += `<tr>
+    <td>${i}</td>
+    <td>${productsContainer[i].name}</td>
+    <td>${productsContainer[i].price}</td>
+    <td>${productsContainer[i].category}</td>
+    <td>${productsContainer[i].description}</td>
+    <td><button class="btn btn-outline-success">update</button></td>
+    <td><button onclick="deleteProduct(${i})" class="btn btn-outline-danger">delete</button></td>
+    </tr>`
+  }
+  document.getElementById('displayBody').innerHTML = dumpProducts;
+}
+
+function deleteProduct(index) {
+  productsContainer.splice(index, 1);
+  localStorage.setItem('ourProducts', JSON.stringify(productsContainer))
+  displayProduct();
+}
+
+function searchProduct(term) {
+  var dumpProducts = ``
+  for (var i = 0; i < productsContainer.length; i++) {
+    if (productsContainer[i].name.toLowerCase().includes(term.toLowerCase()) == true) {
+      dumpProducts += `<tr>
+      <td>${i}</td>
+      <td>${productsContainer[i].name}</td>
+      <td>${productsContainer[i].price}</td>
+      <td>${productsContainer[i].category}</td>
+      <td>${productsContainer[i].description}</td>
+      <td><button class="btn btn-outline-success">update</button></td>
+      <td><button onclick="deleteProduct(${i})" class="btn btn-outline-danger">delete</button></td>
+      </tr>`
+    }
+  }
+  document.getElementById('displayBody').innerHTML = dumpProducts;
+}
+
